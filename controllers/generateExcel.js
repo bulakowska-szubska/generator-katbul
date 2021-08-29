177341e693f1dea243de8858e1ffc0338adf51c1
Kasia <bulakowska.k@gmail.com>
2021-08-29 10:15:51 +0200

    Refactoring kodu

--- controllers/generateExcel.js
+++ controllers/generateExcel.js
@@ -1,36 +1,55 @@
 const ExcelJS = require("exceljs");
 const faker = require("faker");
 
+const monthArray = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+];
+const yearArray = ["2018", "2019", "2020"];
+const yearArray2 = ["2018", "2019", "2020", "2021"];
+const randomDayArray = Array.from(Array(24).keys());
+const currencyArray = [
+  "PLN",
+  "EUR",
+  "USD",
+  "AUD",
+  "DKK",
+  "CAD",
+  "HUF",
+  "CHF",
+  "GBP",
+  "SEK",
+  "CZK",
+  "RUB",
+  "NOK",
+];
+
+const statusArray = ["Active", "Inactive"];
+const numberOfRecords = 10000;
+const numberOfClients = 1000;
+const financeAmount = 10000;
+
 const generateExcelKlienci = function (req, res) {
   let workbook = new ExcelJS.Workbook();
   let worksheet = workbook.addWorksheet("Klienci");
 
-  const monthArray = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-  ];
-  const yearArray = ["2018", "2019", "2020"];
-  const randomDayArray = Array.from(Array(24).keys());
-
-  const statusArray = ["Aktywny", "Zamknięty"];
-
   let arrayOfRows = [];
-  for (let i = 1; i <= 1000; i++) {
+  for (let i = 1; i <= numberOfClients; i++) {
     let randomStatus = Math.floor(Math.random() * statusArray.length);
     let randomMonth = Math.floor(Math.random() * monthArray.length);
     let randomYear = Math.floor(Math.random() * yearArray.length);
     let randomDay = Math.floor(Math.random() * randomDayArray.length);
-    
+
     let randomDate = new Date(
       yearArray[randomYear],
       monthArray[randomMonth],
@@ -70,41 +89,8 @@ const generateExcelUmowy = function (req, res) {
   let workbook = new ExcelJS.Workbook();
   let worksheet = workbook.addWorksheet("Umowy");
 
-  const monthArray = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-  ];
-  const yearArray = ["2018", "2019", "2020", "2021"];
-  const currencyArray = [
-    "PLN",
-    "EUR",
-    "USD",
-    "AUD",
-    "DKK",
-    "CAD",
-    "HUF",
-    "CHF",
-    "GBP",
-    "SEK",
-    "CZK",
-    "RUB",
-    "NOK",
-  ];
-  const randomDayArray = Array.from(Array(24).keys());
-
   let arrayOfRows = [];
-  let numberOfRecords = 10000;
-  let numberOfClients = 1000;
+
   for (let i = 1; i <= numberOfRecords; i++) {
     let randomMonth = Math.floor(Math.random() * monthArray.length);
     let randomYear = Math.floor(Math.random() * yearArray.length);
@@ -125,7 +111,7 @@ const generateExcelUmowy = function (req, res) {
       randomDate,
       monthArray[randomMonth],
       yearArray[randomYear],
-      faker.finance.amount(10, 10000, 2),
+      faker.finance.amount(10, financeAmount, 2),
       currencyArray[randomCurrency],
     ];
     arrayOfRows.push(rowOfMockData);
@@ -151,50 +137,15 @@ const generateExcelWplaty = function (req, res) {
   let workbook = new ExcelJS.Workbook();
   let worksheet = workbook.addWorksheet("Wplaty");
 
-  const monthArray = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-  ];
-  const yearArray = ["2018", "2019", "2020", "2021"];
-  const randomDayArray = Array.from(Array(24).keys());
-
-  const currencyArray = [
-    "PLN",
-    "EUR",
-    "USD",
-    "AUD",
-    "DKK",
-    "CAD",
-    "HUF",
-    "CHF",
-    "GBP",
-    "SEK",
-    "CZK",
-    "RUB",
-    "NOK",
-  ];
-
   let arrayOfRows = [];
-  let numberOfRecords = 2000;
-  let numberOfClients = 1000;
   for (let i = 1; i <= numberOfRecords; i++) {
     let randomCurrency = Math.floor(Math.random() * currencyArray.length);
     let randomMonth = Math.floor(Math.random() * monthArray.length);
-    let randomYear = Math.floor(Math.random() * yearArray.length);
+    let randomYear = Math.floor(Math.random() * yearArray2.length);
     let randomDay = Math.floor(Math.random() * randomDayArray.length);
 
     let randomDate = new Date(
-      yearArray[randomYear],
+      yearArray2[randomYear],
       monthArray[randomMonth],
       randomDayArray[randomDay],
       0,
@@ -205,7 +156,7 @@ const generateExcelWplaty = function (req, res) {
       i,
       Math.floor(Math.random() * numberOfClients + 1),
       randomDate,
-      faker.finance.amount(10, 10000, 2),
+      faker.finance.amount(10, financeAmount, 2),
       currencyArray[randomCurrency],
     ];
     arrayOfRows.push(rowOfMockData);
